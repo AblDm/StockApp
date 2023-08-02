7a8debb5f477b1459cbd645c02c843201b7b3143
AblDm <110186275+AblDm@users.noreply.github.com>
2023-08-02 17:04:31 +0200

    добавил функцию вывода общего числа носков с цветом и содержанием. а нет не оставляю.

--- src/main/java/com/example/stockapp/DTO/SocksRepository.java
+++ src/main/java/com/example/stockapp/DTO/SocksRepository.java
@@ -1,6 +1,7 @@
 package com.example.stockapp.DTO;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,7 +10,10 @@ import java.util.List;
 public interface SocksRepository extends JpaRepository<Socks, Long> {
     List<Socks> findByColorAndCottonPart(String color, int cottonPart);
 
-    int sumQuantityByColorAndCottonPartGreaterThan(String color, int cottonPart);
+    List<Socks> sumQuantityByColorAndCottonPartGreaterThan(String color, int cottonPart);
 
-    int sumQuantityByColorAndCottonPartLessThan(String color, int cottonPart);
+    List<Socks> sumQuantityByColorAndCottonPartLessThan(String color, int cottonPart);
+
+    @Query("SELECT s FROM Socks s WHERE s.color = :color AND s.cottonPart = :cottonPart")
+    List<Socks> findMatchingSocks(String color, int cottonPart);
 }
--- src/main/java/com/example/stockapp/service/SocksServiceImpl.java
+++ src/main/java/com/example/stockapp/service/SocksServiceImpl.java
@@ -9,9 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class SocksServiceImpl implements SocksService {
@@ -72,41 +70,29 @@ public class SocksServiceImpl implements SocksService {
         socksRepository.saveAll(socksList);
     }
 
-    @Override
-    @Transactional
     public List<Socks> getTotalSocks(String color, String operation, int cottonPart) {
+        List<Socks> socksList;
+
         if ("moreThan".equals(operation)) {
-            int totalQuantity = socksRepository.sumQuantityByColorAndCottonPartGreaterThan(color, cottonPart);
-
-            Socks aggregatedSocks = new Socks();
-            aggregatedSocks.setColor(color);
-            aggregatedSocks.setCottonPart(cottonPart);
-            aggregatedSocks.setQuantity(totalQuantity);
-
-            return Collections.singletonList(aggregatedSocks);
+            socksList = socksRepository.findByColorAndCottonPart(color, cottonPart + 1);
         } else if ("lessThan".equals(operation)) {
-            int totalQuantity = socksRepository.sumQuantityByColorAndCottonPartLessThan(color, cottonPart);
-
-            Socks aggregatedSocks = new Socks();
-            aggregatedSocks.setColor(color);
-            aggregatedSocks.setCottonPart(cottonPart);
-            aggregatedSocks.setQuantity(totalQuantity);
-
-            return Collections.singletonList(aggregatedSocks);
+            socksList = socksRepository.findByColorAndCottonPart(color, cottonPart - 1);
         } else if ("equal".equals(operation)) {
-            List<Socks> existingSocks = socksRepository.findByColorAndCottonPart(color, cottonPart);
-
-            if (!existingSocks.isEmpty()) {
-                int totalQuantity = existingSocks.stream().mapToInt(Socks::getQuantity).sum();
-                Socks aggregatedSocks = existingSocks.get(0);
-                aggregatedSocks.setQuantity(totalQuantity);
-                return Collections.singletonList(socksRepository.save(aggregatedSocks));
-            } else {
-                return existingSocks;
-            }
+            socksList = socksRepository.findByColorAndCottonPart(color, cottonPart);
         } else {
-            List<Socks> partialMatches = socksRepository.findByColorAndCottonPart(color, cottonPart);
-            return partialMatches.isEmpty() ? partialMatches : null;
+            throw new InvalidRequestException("Invalid operation: " + operation);
         }
+        Map<String, Socks> socksMap = new HashMap<>();
+        for (Socks socks : socksList) {
+            String key = socks.getColor() + "-" + socks.getCottonPart();
+            if (socksMap.containsKey(key)) {
+                Socks existingSocks = socksMap.get(key);
+                existingSocks.setQuantity(existingSocks.getQuantity() + socks.getQuantity());
+            } else {
+                socksMap.put(key, new Socks(socks.getColor(), socks.getCottonPart(), socks.getQuantity()));
+            }
+        }
+
+        return new ArrayList<>(socksMap.values());
     }
 }
